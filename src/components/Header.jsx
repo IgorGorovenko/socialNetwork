17dd372aa309b0cfbe51f045d62d341df9200ece
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-04 20:16:21 +0200

    add files

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return <header className='header'>
+  <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png' width='240' />
+</header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+    <div>
+      <img src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' width='640'></img>
+    </div>
+    <div>
+      avatar+ description
+    </div>
+    <div>
+      my posts
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    </div>
+  </div>
+      )
+}
+
+export default Profile
