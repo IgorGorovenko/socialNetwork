9f74dc538ba5264fd524b6ab4367435f973b3493
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-10 20:03:26 +0200

    add grid to page dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,30 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Igor
+                </div>
+                <div className={s.dialog}>
+                    Gurman
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are your?</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     )
 
