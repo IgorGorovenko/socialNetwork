f5a84f2d1cb607fa76d465aa662817a8401643f2
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-10 21:21:07 +0200

    refactor code add massive constant dialogsData messagesData postData

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -14,20 +14,33 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Igor'},
+        {id: 2, name: 'Gurman'},
+        {id: 3, name: 'Vova'},
+        {id: 4, name: 'Andrew'},
+        {id: 5, name: 'Sasha'},
+        {id: 6, name: 'Jeka'},
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Fine'},
+        {id: 4, message: 'Ok'},
+        {id: 5, message: 'Ok'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Igor" id="1" />
-                <DialogItem name="Gurman" id="2" />
-                <DialogItem name="Vova" id="3" />
-                <DialogItem name="Andrey" id="4" />
-                <DialogItem name="Sasha" id="5" />
-                <DialogItem name="Jeka" id="6" />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={s.messages}>
-                <Message message='Hi'/>
-                <Message message='How are you?'/>
-                <Message message='Fine'/>
+                <Message message={messagesData[0].message} id={messagesData[0].id}/>
+                <Message message={messagesData[1].message} id={messagesData[1].id}/>
             </div>
         </div>
     )
