8dd28c8c6f604e2db0fcf0ab9ea39f42a6c88bf4
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-11 13:57:22 +0200

    add state.js moved all arrays to the state.js.

---
+++ src/redux/state.js
@@ -0,0 +1,30 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 4},
+            {id: 2, message: 'It is my first post', likesCount: 12},
+            {id: 1, message: 'Hi, how are you?', likesCount: 4},
+            {id: 2, message: 'It is my first post', likesCount: 12},
+        ],
+
+    },
+    dialogsPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'Fine'},
+            {id: 4, message: 'Ok'},
+            {id: 5, message: 'Ok'},
+        ],
+        dialogs: [
+            {id: 1, name: 'Igor'},
+            {id: 2, name: 'Gurman'},
+            {id: 3, name: 'Vova'},
+            {id: 4, name: 'Andrew'},
+            {id: 5, name: 'Sasha'},
+            {id: 6, name: 'Jeka'},
+        ]
+    }
+}
+
+export default state
