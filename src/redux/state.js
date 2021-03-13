5d631e3426e38b8c1f9f3339283f9e174b3f85df
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-13 18:24:35 +0200

    chat add message

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,9 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
+
 let store = {
     _state: {
         profilePage: {
@@ -28,7 +31,9 @@ let store = {
                 {id: 4, name: 'Andrew'},
                 {id: 5, name: 'Sasha'},
                 {id: 6, name: 'Jeka'},
-            ]
+            ],
+            newMessageBody: ''
+
         }
     },
     _callSubscriber() {
@@ -51,18 +56,28 @@ let store = {
             }
 
             this._state.profilePage.posts.push(newPost)
-            this._state.profilePage.newPostText = '';
+            this._state.profilePage.newPostText = ''
             this._callSubscriber(this._state)
         } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-            this._state.profilePage.newPostText = action.newText;
+            this._state.profilePage.newPostText = action.newText
+            this._callSubscriber(this._state)
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+            this._state.dialogsPage.newMessageBody = action.body
+            this._callSubscriber(this._state)
+        } else if (action.type === SEND_MESSAGE) {
+            let body = this._state.dialogsPage.newMessageBody
+            this._state.dialogsPage.newMessageBody = ''
+            this._state.dialogsPage.messages.push({id: 6, message: body})
             this._callSubscriber(this._state)
         }
     }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
-
 export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 
 
 export default store;
