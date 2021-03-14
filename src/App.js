64764f13988e1430683ab4bdbf1a5eb0beea9df4
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-14 13:34:48 +0200

    ERROR

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom"
 
 const App = (props) => {
-    debugger
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -19,7 +18,7 @@ const App = (props) => {
                     <Route path='/dialogs'
                            render={() => <Dialogs store={props.store} />}/>
                     <Route path='/profile'
-                           render={() => <Profile profilePage={props.state.profileReducer}
+                           render={() => <Profile profilePage={props.state.profilePage}
                                                   dispatch={props.dispatch}/>}/>
                     <Route path='/news'
                            render={() => <News/>}/>
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,9 +2,9 @@ import s from './ProfileInfo.module.css';
 
 const ProfileInfo = () => {
     return (
-        <div >
+        <div>
             <div>
-                <img src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' height='320'/>
+                <img src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' height='120'/>
             </div>
             <div className={s.descriptionBlock}>
                 avatar+ description
