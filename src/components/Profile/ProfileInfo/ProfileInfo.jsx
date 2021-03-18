9832bb0019d42fc607c8f982cc1ffde02bf170d1
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-19 00:23:38 +0200

    profile page upd

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,19 @@
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if(!props.profile) {
+        return <Preloader/>
+    }
+
     return (
         <div >
             <div>
                 <img src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' height='120'/>
             </div>
             <div className={s.descriptionBlock}>
-                avatar+ description
+                <img src={props.profile.photos.large}/>
+                avatar + description
             </div>
         </div>
     )
