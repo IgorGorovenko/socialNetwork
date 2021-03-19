63d7127827358a9c3f9e2c3341c8d257d4c3fd81
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-19 20:18:58 +0200

    login error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-  return <header className={s.header}>
-  <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png' width='240' />
-</header>
+const Header = (props) => {
+    return <header className={s.header}>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png' width='240'/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
+    </header>
 }
 
 export default Header;
