1eb5b3cf3c9e52fe694c437a2b88c8ce68d1f47d
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-20 12:35:43 +0200

    add DAL

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "263df062-c1ff-4450-bbbe-870baa90759c"
+    }
+
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        })
+}
