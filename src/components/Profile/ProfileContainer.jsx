0a3d3316d9172ef4b1873bc9a30e248898a1445e
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-20 17:52:05 +0200

    add redirect

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,6 +4,7 @@ import * as axios from "axios";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../redux/profileReducer";
 import {withRouter} from "react-router";
+import {Redirect} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
@@ -16,6 +17,8 @@ class ProfileContainer extends React.Component {
     }
 
     render() {
+        if (!this.props.isAuth) return <Redirect to='/login'/>
+
         return (
             <Profile {...this.props} profile={this.props.profile}/>
         )
@@ -23,7 +26,8 @@ class ProfileContainer extends React.Component {
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    isAuth: state.auth.isAuth
 })
 
 let WithUrlDataContainerComponents = withRouter(ProfileContainer)
