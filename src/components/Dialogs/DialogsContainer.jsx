d25e8b1d273d90c2e5dcc6533cf56fb6e7581a69
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-21 10:42:16 +0200

    upd form redux-form

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogsReducer";
+import {sendMessageCreator} from "../../redux/dialogsReducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
@@ -13,12 +13,9 @@ let mapStateToProps = (state) => {
 }
 let mapDispatchToProps = (dispatch) => {
     return {
-        sendMessage: () => {
-            dispatch(sendMessageCreator())
+        sendMessage: (newMessageBody) => {
+            dispatch(sendMessageCreator(newMessageBody))
         },
-        updateNewMessageBody: (body) => {
-            dispatch(updateNewMessageBodyCreator(body))
-        }
     }
 }
 
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,7 +1,6 @@
 import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST'
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
 
@@ -13,7 +12,6 @@ let initialState = {
         {id: 1, message: 'Hi, how are you?', likesCount: 4},
         {id: 2, message: 'It is my first post', likesCount: 12},
     ],
-    newPostText: 'Gurman',
     profile: null,
     status: ''
 
@@ -24,7 +22,7 @@ const profileReducer = (state = initialState, action) => {
         case ADD_POST: {
             let newPost = {
                 id: 5,
-                message: state.newPostText,
+                message: action.newPostText,
                 likesCount: 0,
             };
             return {
@@ -33,13 +31,7 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: ''
             }
         }
-        case UPDATE_NEW_POST_TEXT: {
-            return {
-                ...state,
-                newPostText: action.newText
 
-            }
-        }
         case SET_STATUS: {
             return {
                 ...state,
@@ -56,7 +48,7 @@ const profileReducer = (state = initialState, action) => {
     }
 }
 
-export const addPostActionCreator = () => ({type: ADD_POST})
+export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
 
@@ -82,8 +74,5 @@ export const updateStatus = (status) => (dispatch) => {
             }
         })
 }
-
-export const updateNewPostTextActionCreator = (text) => (
-    {type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export default profileReducer
