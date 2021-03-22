5259dd82c00f458c8ef0143eaad07df529971032
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-23 00:29:27 +0200

    add extebsion for ghghrrome

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import sitebarReducer from "./sitebarReducer";
@@ -9,17 +9,18 @@ import {reducer as formReducer} from 'redux-form'
 import appReducer from "./appReducer";
 
 let reducers = combineReducers({
-    profilePage: profileReducer,
-    dialogsPage: dialogsReducer,
-    sitebar: sitebarReducer,
-    usersPage: usersReducer,
-    auth: authReducer,
-    form: formReducer,
-    app: appReducer
-})
+        profilePage: profileReducer,
+        dialogsPage: dialogsReducer,
+        sitebar: sitebarReducer,
+        usersPage: usersReducer,
+        auth: authReducer,
+        form: formReducer,
+        app: appReducer
+    })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store
+window.__store__ = store
 
 export default store
