6dcf49b877fa6f4340ee3cc4d719449bf486b62c
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-22 15:35:19 +0200

    hook, useState

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -48,7 +48,7 @@ class ProfileStatus extends React.Component {
                 <div>
                     <input onChange={this.onStatusChange}
                            autoFocus={true}
-                           onBlur={this.deactivateEditMode}
+                           onBlur={this.deactivateEditMode.bind(this)}
                            value={this.state.status}/>
                 </div>
                 }
