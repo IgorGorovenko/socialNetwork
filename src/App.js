af2c3920b537f91ff96efdcbd0ac92170ea474c5
Igor Gorovenko <59226858+IgorGorovenko@users.noreply.github.com>
2021-03-23 20:08:00 +0200

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import News from "./components/News/News"
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route, withRouter} from "react-router-dom"
+import {BrowserRouter, Route, withRouter, Switch, Redirect} from "react-router-dom"
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
@@ -31,23 +28,24 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <div className='app-wrapper'>
+
                     <HeaderContainer/>
                     <Navbar/>
+
                     <div className='app-wrapper-content'>
-                        <Route path='/dialogs'
-                               render={() => <DialogsContainer/>}/>
-                        <Route path='/profile/:userId?'
-                               render={() => <ProfileContainer/>}/>
-                        <Route path='/users'
-                               render={() => <UsersContainer/>}/>
-                        <Route path='/login'
-                               render={() => <LoginPage/>}/>
-                        <Route path='/news'
-                               render={() => <News/>}/>
-                        <Route path='/music'
-                               render={() => <Music/>}/>
-                        <Route path='/settings'
-                               render={() => <Settings/>}/>
+                        <Switch>
+                            <Route path='/dialogs'
+                                   render={() => <DialogsContainer/>}/>
+                            <Route path='/profile/:userId?'
+                                   render={() => <ProfileContainer/>}/>
+                            <Route path='/users'
+                                   render={() => <UsersContainer/>}/>
+                            <Route path='/login'
+                                   render={() => <LoginPage/>}/>
+
+                            <Route path='*'
+                                   render={() => <div>404 NOT FOUND</div>}/>
+                        </Switch>
                     </div>
                 </div>
             </BrowserRouter>
