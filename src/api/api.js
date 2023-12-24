88587c5779b3308bf02eb3a77dc37ebbe0ad6647
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-24 11:26:36 +0200

    upd securite key, upd readme

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        "API-KEY": "263df062-c1ff-4450-bbbe-870baa90759c"
+        "API-KEY": "557c9420-40a6-4a00-bd67-0da0d47966fb"
     }
 
 })
